9c7c8307a9717c717d4994527846e971f728d21d
mr-ronny <trialjunctiondavid0@gmail.com>
2024-03-22 19:30:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,72 @@
+//  Smooth scroll
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+    var typed = new Typed('#element', {
+      strings: ['web developer.', "digital marketing ||seo.","web designer.","Nodejs |Reactjs| Sql|","Javascript| Python||C","Html ||Css||cyber security" ],
+      typeSpeed: 50,
+    });
+
+
+Shery.mouseFollower({
+  //Parameters are optional.
+  skew: true,
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  duration: 1,
+});
+
+
+Shery.makeMagnet(".home" /* Element to target.*/, {
+  //Parameters are optional.
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  duration: 1,
+});
+
+Shery.makeMagnet(".btn" /* Element to target.*/, {
+  //Parameters are optional.
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  duration: 1,
+});
+
+Shery.makeMagnet(".home" /* Element to target.*/, {
+  //Parameters are optional.
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  duration: 1,
+});
+
+
+Shery.imageEffect(".rightsec", {style : 5, debug: true, gooey: true})
+
+
+              /* //page 2 animation */
+
+
+var elemc = document.querySelector("#elem-container")
+var fixed = document.querySelector("#fixed-image")
+elemc.addEventListener("mouseenter",function(){
+  fixed.style.display = "block"
+  // alert("hssdsd")
+})
+
+elemc.addEventListener("mouseleave",function(){
+  fixed.style.display = "none"
+})
+
+
+var elems = document.querySelectorAll(".elem")
+elems.forEach(function(e){
+  e.addEventListener("mouseenter",function(){
+    var image = e.getAttribute("data-image")
+    fixed.style.backgroundImage = `url(${image})`
+    fixed.style.backgroundImage = `url(${image})`
+    // console.log(image)
+  })
+})
+
+                // loader Animation
+
+var loader = document.querySelector("#loader")
+setTimeout(function(){
+  loader.style.top = "-100%"
+},8500)                
