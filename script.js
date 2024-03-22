307d42c161f5645f71e8685e69545528118443d1
mr-ronny <trialjunctiondavid0@gmail.com>
2024-03-22 19:46:05 +0530

    changes

--- script.js
+++ script.js
@@ -3,70 +3,67 @@ const scroll = new LocomotiveScroll({
   el: document.querySelector("#main"),
   smooth: true
 });
-    var typed = new Typed('#element', {
-      strings: ['web developer.', "digital marketing ||seo.","web designer.","Nodejs |Reactjs| Sql|","Javascript| Python||C","Html ||Css||cyber security" ],
-      typeSpeed: 50,
-    });
+var typed = new Typed('#element', {
+  strings: ['web developer.', "digital marketing ||seo.", "web designer.", "Nodejs |Reactjs| Sql|", "Javascript| Python||C", "Html ||Css||cyber security"],
+  typeSpeed: 50,
+});
 
 
 Shery.mouseFollower({
-  //Parameters are optional.
+
   skew: true,
   ease: "cubic-bezier(0.23, 1, 0.320, 1)",
   duration: 1,
 });
 
 
-Shery.makeMagnet(".home" /* Element to target.*/, {
-  //Parameters are optional.
+Shery.makeMagnet(".home", {
   ease: "cubic-bezier(0.23, 1, 0.320, 1)",
   duration: 1,
 });
 
-Shery.makeMagnet(".btn" /* Element to target.*/, {
-  //Parameters are optional.
+Shery.makeMagnet(".btn", {
   ease: "cubic-bezier(0.23, 1, 0.320, 1)",
   duration: 1,
 });
 
-Shery.makeMagnet(".home" /* Element to target.*/, {
-  //Parameters are optional.
+Shery.makeMagnet(".home", {
   ease: "cubic-bezier(0.23, 1, 0.320, 1)",
   duration: 1,
 });
 
 
-Shery.imageEffect(".rightsec", {style : 5, debug: true, gooey: true})
+Shery.imageEffect(".rightsec", { style: 5, debug: true, gooey: true })
 
 
-              /* //page 2 animation */
+/* //page 2 animation */
 
 
 var elemc = document.querySelector("#elem-container")
 var fixed = document.querySelector("#fixed-image")
-elemc.addEventListener("mouseenter",function(){
+elemc.addEventListener("mouseenter", function () {
   fixed.style.display = "block"
   // alert("hssdsd")
 })
 
-elemc.addEventListener("mouseleave",function(){
+elemc.addEventListener("mouseleave", function () {
   fixed.style.display = "none"
 })
 
 
 var elems = document.querySelectorAll(".elem")
-elems.forEach(function(e){
-  e.addEventListener("mouseenter",function(){
+elems.forEach(function (e) {
+  e.addEventListener("mouseenter", function () {
     var image = e.getAttribute("data-image")
     fixed.style.backgroundImage = `url(${image})`
     fixed.style.backgroundImage = `url(${image})`
-    // console.log(image)
+
   })
 })
 
-                // loader Animation
+// loader Animation
 
 var loader = document.querySelector("#loader")
-setTimeout(function(){
+setTimeout(function () {
   loader.style.top = "-100%"
-},8500)                
+}, 8500)                
